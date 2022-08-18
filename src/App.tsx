392109c97e91c6661037bcbf01df168122e6754c
puxiao <yangpuxiao@gmail.com>
2022-08-19 00:03:48 +0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.scss'
 //import SimpleTriangle from '@/components/simple-triangle'
 //import VertexBuffrSlot from './components/vertex-buffer-slot'
 //import XYZVertexBufferSlot from './components/xy-z-vertex-buffer-slot'
-
-import FragBindGroup from './components/frag-bind-group'
+//import FragBindGroup from './components/frag-bind-group'
+import HeartShape from './components/heart-shape'
 
 function App() {
     return (
@@ -14,9 +14,10 @@ function App() {
             <SimpleTriangle />
             <VertexBuffrSlot />
             <XYZVertexBufferSlot />
+            <FragBindGroup />
              */}
 
-             <FragBindGroup />
+             <HeartShape />
         </Fragment>
     )
 }
