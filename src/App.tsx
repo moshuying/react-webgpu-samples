a75c400ca0ac8f2a5d809298891b72ee4ff2c79c
puxiao <yangpuxiao@gmail.com>
2022-09-04 22:44:08 +0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Fragment } from 'react'
 import './App.scss'
+import ColorInterpolation from './components/color-interpolation'
 
 //import SimpleTriangle from '@/components/simple-triangle'
 //import VertexBuffrSlot from './components/vertex-buffer-slot'
 //import XYZVertexBufferSlot from './components/xy-z-vertex-buffer-slot'
 //import FragBindGroup from './components/frag-bind-group'
 //import HeartShape from './components/heart-shape'
-import NDCTriangle from './components/ndc-triangle'
+//import NDCTriangle from './components/ndc-triangle'
 
 function App() {
     return (
@@ -17,9 +18,10 @@ function App() {
             <XYZVertexBufferSlot />
             <FragBindGroup />
             <HeartShape />
+            <NDCTriangle />
              */}
 
-             <NDCTriangle />
+            <ColorInterpolation />
         </Fragment>
     )
 }
